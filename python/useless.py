b876890268623d4832258dc31282644ece147cc8
Dominic-Infant-Raj <168755969+Dominic-Infant-Raj@users.noreply.github.com>
2024-05-14 22:01:22 +0530

    Update useless.py
    Making the code more useless by just adding useless and unnecessary lines

--- python/useless.py
+++ python/useless.py
@@ -1,2 +1,12 @@
+class Useless(): # just making the code more useless by adding unessasary lines
+    """The most useless class on earth"""
+    def __init__(self,text):
+        """
+        the most useless constructor for the most useless class
+        Args:
+            text (string): the string u want the class to print
+        """
+        print(text)
+
 while True:
-  print("useless")
+    Useless(text = "Useless")
